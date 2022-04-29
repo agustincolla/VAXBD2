c21e568353987b1ddc8657d88cbb4f1cfef3aecc
agucoyote <agu_coyote@hotmail.com>
2022-04-29 05:50:01 -0300

    se agrego comportamiento a vaxRepository

--- src/main/java/ar/edu/unlp/info/bd2/repositories/VaxRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/VaxRepository.java
@@ -1,5 +1,17 @@
 package ar.edu.unlp.info.bd2.repositories;
 
-public class VaxRepository {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
+public class VaxRepository {
+  public SessionFactory sessionFactory= new Configuration().configure().buildSessionFactory() ;
+  public Session session= sessionFactory.openSession();
+  public void save(Object guardar) {
+	  session.beginTransaction();
+	  session.save(guardar);
+	  session.getTransaction().commit();
+	  session.close();
+  }
+  
 }
