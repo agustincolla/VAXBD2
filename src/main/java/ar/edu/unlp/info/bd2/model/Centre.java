47ec836b241f839313347199c36017d1b893539b
Mariela M <medinta.mm@gmail.com>
2022-05-20 20:04:39 -0300

    Se agregó Centro, Shot, Esq de Vacunación

--- src/main/java/ar/edu/unlp/info/bd2/model/Centre.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Centre.java
@@ -1,5 +1,12 @@
 package ar.edu.unlp.info.bd2.model;
 
+import java.util.ArrayList;
+
 public class Centre {
+	Long id;
 	String name;
+	ArrayList<Nurse> nurses;
+	ArrayList<SupportStaff> staff;
+	
+	
 }
--- src/main/java/ar/edu/unlp/info/bd2/model/VaccinationSchedule.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/VaccinationSchedule.java
@@ -4,7 +4,26 @@ import java.util.ArrayList;
 
 public class VaccinationSchedule {
  ArrayList<Vaccine> vaccine;
- // getId()
- // addVaccine ()
+ Long Id;
+  
  // getVaccines()
+public ArrayList<Vaccine> getVaccine() {
+	return vaccine;
+}
+// setVaccine ()
+public void setVaccine(ArrayList<Vaccine> vaccine) {
+	this.vaccine = vaccine;
+}
+//getId()
+public Long getId() {
+	return Id;
+}
+// setId
+public void setId(Long id) {
+	Id = id;
+}
+
+public VaccinationSchedule(ArrayList<Vaccine> vaxs)  {
+	this.setVaccine(vaxs);
+}
 }
