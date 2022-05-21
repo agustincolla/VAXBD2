fcf52106156e3d6316284fac1cdedf48d5d4500c
Mariela M <medinta.mm@gmail.com>
2022-05-20 21:00:06 -0300

    Se agregó comportamiento a centro y paciente

--- src/main/java/ar/edu/unlp/info/bd2/model/Centre.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Centre.java
@@ -5,8 +5,48 @@ import java.util.ArrayList;
 public class Centre {
 	Long id;
 	String name;
-	ArrayList<Nurse> nurses;
-	ArrayList<SupportStaff> staff;
+	ArrayList<Nurse> nurses = new ArrayList<Nurse>();
+	ArrayList<SupportStaff> staffs = new ArrayList<SupportStaff> (); 
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Nurse> getNurses() {
+		return nurses;
+	}
+	public void setNurses(ArrayList<Nurse> nurses) {
+		this.nurses = nurses;
+	}
+	public ArrayList<SupportStaff> getStaffs() {
+		return staffs;
+	}
+	public void setStaffs(ArrayList<SupportStaff> staffs) {
+		this.staffs = staffs;
+	}
 	
+	public void addStaff (SupportStaff staffAux){
+		if (staffAux.addCentre(this)){
+			this.getStaffs().add(staffAux);
+		}
+		
+	}
+	
+	public void addNurse (Nurse nurse){
+		if (nurse.addCentre(this)) {
+			this.getNurses().add(nurse);
+		}
+	}
+	
+	public Centre (String name) {
+		this.setName(name);
+	}
 }
--- src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
+++ src/main/java/ar/edu/unlp/info/bd2/model/Patient.java
@@ -3,8 +3,47 @@ package ar.edu.unlp.info.bd2.model;
 import java.util.Date;
 
 public class Patient {
+	Long id;
 	String email; 
 	String fullname;
 	String password; 
 	Date dayOfBirth;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getFullname() {
+		return fullname;
+	}
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Date getDayOfBirth() {
+		return dayOfBirth;
+	}
+	public void setDayOfBirth(Date dayOfBirth) {
+		this.dayOfBirth = dayOfBirth;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Patient (String mail, String name, String pass, Date fecha) {
+		this.setEmail(mail);
+		this.setFullname(name);
+		this.setPassword(pass);
+		this.setDayOfBirth(fecha);
+	}
 }
