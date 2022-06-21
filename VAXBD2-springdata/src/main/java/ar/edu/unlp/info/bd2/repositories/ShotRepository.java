4baab5b25b2cde062ca3b370d61d7e30ec41ce2c
Agustin colla <agu_coyote@hotmail.com>
2022-06-21 20:35:02 -0300

    se agrego el repositorio para el crud de las clases que faltaban, por ahora son todas interfases vacias

---
+++ VAXBD2-springdata/src/main/java/ar/edu/unlp/info/bd2/repositories/ShotRepository.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.info.bd2.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unlp.info.bd2.model.Shot;
+
+public interface ShotRepository extends CrudRepository<Shot, Long>{
+
+}
